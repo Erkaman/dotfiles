5a7dc2a377e0e30fa5d03f17ceb14578b747807c
Eric Arnebäck <erkastina@hotmail.com>
2012-10-17 17:57:21 +0200

    Added glsl-mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1123,3 +1123,8 @@ If no associated application, then `find-file' FILE."
             (setq indent-tabs-mode)))
 
 (require 'hwflisp-mode)
+
+; glsl mode
+(autoload 'glsl-mode "glsl-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
